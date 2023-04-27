a432ae0ac512743127476114c315b7ba2acac781
omar <omarmc2@gmail.com>
2023-04-26 19:38:35 -0600

    agregando metatag OG description

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import { GTMscript } from './lib/GTMscript'
 export const metadata = {
   title: 'Apoya la integridad electoral #MiVotoNoSeToca | Plataforma para enviar mensajes de defensa del INE',
   description: 'Únete a la campaña #MiVotoNoSeToca y apoya la integridad electoral en México. Envía mensajes de defensa del INE a través de nuestra plataforma. ¡Participa ahora!',
+  ogDescription:'Únete a la campaña #MiVotoNoSeToca y apoya la integridad electoral en México. Envía mensajes de defensa del INE a través de nuestra plataforma. ¡Participa ahora!'
 }
 
 export default function RootLayout({ children }) {
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="es">
     <head>
     <link rel="shortcut icon" type="image/x-icon" href="https://images.squarespace-cdn.com/content/v1/63eb23b7e33e0b09a85385b4/fd64a92c-b011-4814-9310-97bd1820ddd5/favicon.ico"/>
+    <meta property="og:description" content="Únete a la campaña #MiVotoNoSeToca y apoya la integridad electoral en México. Envía mensajes de defensa del INE a través de nuestra plataforma. ¡Participa ahora!"></meta>
       <GTMscript/>
       <GAscript />
     </head>
