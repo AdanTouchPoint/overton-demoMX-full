76d930c0dcd64b0cf10f19a6f23ab11da7657088
omar <omarmc2@gmail.com>
2023-04-26 19:24:04 -0600

    cambiando atributo lang a español

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
     <head>
     <link rel="shortcut icon" type="image/x-icon" href="https://images.squarespace-cdn.com/content/v1/63eb23b7e33e0b09a85385b4/fd64a92c-b011-4814-9310-97bd1820ddd5/favicon.ico"/>
       <GTMscript/>
