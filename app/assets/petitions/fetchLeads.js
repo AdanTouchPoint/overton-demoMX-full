2d314977ae44818c0595a1796fbfb9681cb4a180
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2023-04-26 20:08:18 -0500

    add subject to leads post

--- app/assets/petitions/fetchLeads.js
+++ app/assets/petitions/fetchLeads.js
@@ -1,7 +1,7 @@
 import { fetchData } from "./fetchData"
 const fetchLeads = (successResponse, backendURLBase, endpoints, clientId, dataUser, emailData) =>{
         
-    fetchData('POST', backendURLBase, endpoints.toSaveLeads, clientId,`&firstName=${dataUser.userName ? dataUser.userName:''}&postalcode=${dataUser.zipCode ? dataUser.zipCode:dataUser.state}&emailData=${dataUser.emailUser}&representative=${emailData.name}&emailMessage=${dataUser.text}&city=${emailData.state}&party=${emailData.party}&sended=${successResponse}`)
+    fetchData('POST', backendURLBase, endpoints.toSaveLeads, clientId,`&firstName=${dataUser.userName ? dataUser.userName:''}&postalcode=${dataUser.zipCode ? dataUser.zipCode:dataUser.state}&emailData=${dataUser.emailUser}&representative=${emailData.name}&subject=${emailData.subject}&emailMessage=${dataUser.text}&city=${emailData.state}&party=${emailData.party}&sended=${successResponse}`)
     // axios.post(`https://payload-demo-tpm.herokuapp.com/leads?&firstName=${dataUser.userName ? dataUser.userName:''}&postalcode=${dataUser.zipCode ? dataUser.zipCode:dataUser.state}&emailData=${dataUser.emailUser}&representative=${emailData.name}&emailMessage=${dataUser.text}&city=${emailData.state}&party=${emailData.party}&clientId=${clientId}&sended=${respuestaDeExito}`, dataUseryEmail)
 }
 
